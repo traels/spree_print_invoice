de52fd3bf0b183e78c71853e7de6b359b9025d44
erwin <yves.dufour@me.com>
2013-04-13 01:07:04 +0200

    update admin orders controller decorator
    store/restore current locale

--- app/controllers/spree/admin/orders_controller_decorator.rb
+++ app/controllers/spree/admin/orders_controller_decorator.rb
@@ -1,13 +1,28 @@
 Spree::Admin::OrdersController.class_eval do
-  respond_to :pdf
+
+  before_filter :save_current_locale , :only => :show
+  after_filter :current_locale , :only => :show
 
   def show
     load_order
     respond_with(@order) do |format|
       format.pdf do
         template = params[:template] || "invoice"
-        render :layout => false , :template => "spree/admin/orders/#{template}.pdf.prawn"
+        I18n.locale = params[:language].to_sym unless params[:language].nil?
+        page_size = Spree::PrintInvoice::Config[:page_size]
+        page_layout = Spree::PrintInvoice::Config[:page_layout].to_sym
+        pdf = Prawn::Document.new(:page_size => page_size, :page_layout => page_layout)
+        render :pdf => pdf, :layout => false, :template => "spree/admin/orders/#{template}.pdf.prawn"
       end
     end
   end
+  
+  def save_current_locale
+      @current_locale = I18n.locale
+  end
+  def current_locale
+      I18n.locale = @current_locale
+  end
+
 end
+
