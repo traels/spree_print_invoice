d803ea52392ed18c628bd11c9649ec2259476240
Tobias Bohwalli <hi@futhr.io>
2013-07-20 15:47:46 +0200

    Add base spec containers.

---
+++ spec/controllers/spree/admin/order_controller_decorator_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe Spree::Admin::OrdersController do
+  stub_authorization!
+
+  let(:user) { create(:user) }
+
+  before { controller.stub spree_current_user: user }
+
+  context "#show" do
+    specify do
+    end
+  end
+
+  context "#save_current_locale" do
+    specify do
+    end
+  end
+
+  context "#current_locale" do
+    specify do
+    end
+  end
+end
---
+++ spec/features/spree/admin/print_invoice_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+feature "Print Invoice" do
+  stub_authorization!
+
+  background do
+    2.times { create(:order) }
+    # visit spree.admin_path
+    # page!
+  end
+
+  scenario "can print" do
+  end
+end
