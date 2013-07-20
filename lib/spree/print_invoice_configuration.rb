0f1af200487235938c97e5beb4d70d311b83ebac
Tobias Bohwalli <hi@futhr.io>
2013-07-20 06:39:06 +0200

    Cleanup.

--- lib/spree/print_invoice_configuration.rb
+++ lib/spree/print_invoice_configuration.rb
@@ -1,6 +1,6 @@
 module Spree
-  class PrintInvoiceConfiguration < Spree::Preferences::Configuration
-    preference :print_invoice_logo_path, :string, default: Spree::Config.admin_interface_logo
+  class PrintInvoiceConfiguration < Preferences::Configuration
+    preference :print_invoice_logo_path, :string, default: Spree::Config[:admin_interface_logo]
     preference :print_buttons,           :string, default: 'invoice'
     preference :page_size,               :string, default: 'LETTER'
     preference :page_layout,             :string, default: 'landscape'
