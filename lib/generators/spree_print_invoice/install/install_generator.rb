3693190867d1013458a850153545a78f86daf859
Tobias Bohwalli <hi@futhr.io>
2013-07-30 21:04:31 +0200

    Fixes select2 styling and language names.
    To be able to choose locale now relies on that the spree_i18n gem is
    installed.

--- lib/generators/spree_print_invoice/install/install_generator.rb
+++ lib/generators/spree_print_invoice/install/install_generator.rb
@@ -4,6 +4,10 @@ module SpreePrintInvoice
       def add_javascripts
         append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_print_invoice\n"
       end
+
+      def add_stylesheets
+        inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_print_invoice\n", before: /\*\//, verbose: true
+      end
     end
   end
 end
