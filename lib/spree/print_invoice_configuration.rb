f0b4326cc4192d5866a280fded79eac7a2effb58
Tobias Bohwalli <hi@futhr.io>
2013-07-30 15:56:06 +0200

    Add packaging_slip with defaults in preferences.

--- lib/spree/print_invoice_configuration.rb
+++ lib/spree/print_invoice_configuration.rb
@@ -1,7 +1,7 @@
 module Spree
   class PrintInvoiceConfiguration < Preferences::Configuration
     preference :print_invoice_logo_path, :string, default: Spree::Config[:admin_interface_logo]
-    preference :print_buttons,           :string, default: 'invoice'
+    preference :print_buttons,           :string, default: 'invoice,packaging_slip'
     preference :page_size,               :string, default: 'LETTER'
     preference :page_layout,             :string, default: 'landscape'
   end
