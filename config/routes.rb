bb362b555f6096adf6c1a0bc7b46277b05540681
Tobias Bohwalli <hi@futhr.io>
2013-08-01 03:36:48 +0200

    Append routes or it with create route issues.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Spree::Core::Engine.routes.draw do
+Spree::Core::Engine.routes.append do
   namespace :admin do
     match '/orders/:id(.:format)', to: 'orders#show', via: :get
     resource :print_invoice_settings, only: [:edit, :update]
